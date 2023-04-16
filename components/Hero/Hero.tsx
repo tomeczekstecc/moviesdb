e47d63857ef333b4eb60f4b6ab464d51b2416d3b
tomek <tomeczekstecc@gmail.com>
2023-04-16 19:49:19 +0200

    fixes

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Hero = ({imgUrl, title, text}: Props) =>
     <div className='relative w-full h-128'>
-        <div className='relative flex flex-col-reverse h-full max-w-7xl m-auto z-10 pb-12 text-center md:text-left'>Hero
+        <div className='relative flex flex-col-reverse h-full max-w-7xl m-auto z-10 pb-12 text-center md:text-left'>
             <div className="text-white max-w-2xl px-4">
                 <h2 className='text-2xl md:text-5xl font-bold pb-8'>{title}</h2>
                 <p className='text-lg md:text-xl'>{text}</p>
--- components/Spinner/Spinner.tsx
+++ components/Spinner/Spinner.tsx
@@ -1,3 +1,14 @@
-const Spinner = () => <div>Spinner</div>;
+const Spinner = () =>
+    <div>
+        <div className="spinner-border text-primary" role="status">
+            <svg className="spinner-grow" width="3em" height="3em" viewBox="0 0 16 16"
+                 fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd"
+                      d="M8 1a7 7 0 0 1 7 7v1.5a.5.5 0 0 1-1 0V8a6 6 0 0 0-6-6 5.998 5.998 0 0 0-5.657 4H1.5a.5.5 0 0 1 0-1h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h1A7 7 0 0 1 8 1z"/>
+                <path fillRule="evenodd"
+                      d="M8 15a7 7 0 0 1-7-7v-1.5a.5.5 0 0 1 1 0V8a6 6 0 0 0 6 6 5.998 5.998 0 0 0 5.657-4H14.5a.5.5 0 0 1 0 1h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a7 7 0 0 1-7 1z"/>
+            </svg>
+        </div>
+    </div>;
 
 export default Spinner;
